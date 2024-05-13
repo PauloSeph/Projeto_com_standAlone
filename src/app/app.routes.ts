72a0a8aca79327ccd49ab7bdd4cbfc24142f91e4
seph <paulo.seph@hotmai.com>
2024-05-13 13:22:40 -0300

    Test de Requisições com Standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CursoComponent } from './features/curso/pages/cursos/curso.component';
+import { HomeComponent } from './features/home/home.component';
+import { FormCursoComponent } from './features/curso/pages/formCurso/formCurso.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+
+
+  { path: 'curso', component: CursoComponent},
+  { path: 'curso/cadastrar', component: FormCursoComponent}
+];
---
+++ src/app/features/curso/models/Icurso.ts
@@ -0,0 +1,6 @@
+export interface ICurso{
+  nome: string,
+  preco: number
+}
+
+
